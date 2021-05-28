c92608164f14b59435c08a649099355e27ff0e6d
Stranger-S <Stranger_90@ukr.net>
2021-05-28 12:34:23 +0300

    Lesson 10

---
+++ Lesson 10/script.js
@@ -0,0 +1,39 @@
+let dbclick = document.querySelector('.block');
+let number = 0;
+dbclick.ondblclick = function() {
+    console.log('dbclick');
+    number = number + 1;
+    document.querySelector('.number').innerHTML = "<br>" + number;
+}
+dbclick.onmousedown = function() {
+    return false;
+}
+
+let sq = document.querySelector('.sq');
+let left = 0;
+let up = 0;
+document.onkeydown = function(e){
+    //console.log(e);
+    if(e.key == "ArrowRight") {
+        left = left + 10;
+        sq.style.marginLeft = left + 'px';
+    }
+    if(e.key == "ArrowLeft") {
+        left = left - 10;
+        sq.style.marginLeft = left + 'px';
+    }
+    if(e.key == "ArrowUp") {
+        up = up - 10;
+        sq.style.marginTop = up + 'px';
+    }
+    if(e.key == "ArrowDown") {
+        up = up + 10;
+        sq.style.marginTop = up + 'px';
+    }
+    if(e.key == "Delete") {
+        left = 0;
+        up = 0;
+        sq.style.marginLeft = left + 'px';
+        sq.style.marginTop = up + 'px';
+    }
+}
