f6513b326de53ab368b88f1148a8d6489c30ef66
Stranger-S <Stranger_90@ukr.net>
2021-06-04 20:07:33 +0300

    modified

--- Lesson 12/12.2/script.js
+++ Lesson 12/12.2/script.js
@@ -3,6 +3,12 @@ function openMenu() {
     let menuId = this.dataset.menu;
     document.querySelector(menuId).parentElement.classList.remove('close');
     document.querySelector(menuId).classList.remove('close');
+    document.onkeydown = function(p){
+        if(p.key == 'Escape') {
+            document.querySelector('.menu').classList.add('close');
+            document.querySelector('.menu-list').classList.add('close'); 
+        }
+    }
 }
 
 document.querySelector('.menu-close').onclick = closeMenu;
@@ -12,8 +18,25 @@ function closeMenu() {
     document.querySelector(menuId).classList.add('close');
 }
 
-document.querySelector('.menu').onclick = hideMenu;
+document.onclick = function(e){
+    if(e.target == document.querySelector('.menu')){
+        document.querySelector('.menu').classList.add('close');
+        document.querySelector('.menu-list').classList.add('close');
+    }
+}
+
+/*document.addEventListener('click', function(event){
+    let menu = document.querySelector('.menu');
+    console.log(event.target);
+    if(event.target == menu){
+        document.querySelector('.menu').classList.add('close');
+        document.querySelector('.menu-list').classList.add('close');
+    }
+})*/
+
+//----------Изначальный вариант----------------
+/*document.querySelector('.menu').onclick = hideMenu;
 function hideMenu() {
     this.classList.add('close');
     this.children[0].classList.add('close');
-}
+}*/
