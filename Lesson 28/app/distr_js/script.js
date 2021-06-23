05b4a1bddccc5c37d666daf7ffe85688d23ab370
Stranger-S <Stranger_90@ukr.net>
2021-06-23 10:49:24 +0300

    lesson 28

---
+++ Lesson 28/app/distr_js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var a = "Stranger";
+var b = "hello " + a + ", nice to meet you!";
+console.log(b);
---
+++ Lesson 28/app/src_js/script.js
@@ -0,0 +1,3 @@
+let a = "Stranger";
+let b = `hello ${a}, nice to meet you!`;
+console.log(b);
